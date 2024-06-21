8ff879e2481f06e4fd29e35e7913bee360ffa8f3
DELL <sulaimabaliqis@gmail.com>
2024-06-21 09:11:00 +0100

    auction

--- auction_sales/models.py
+++ auction_sales/models.py
@@ -4,23 +4,23 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.IntegerField(max_length=55, primary_key=True, unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=False, null=False)
+    email = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    username = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    password = models.CharField(max_length=20, null=False, blank=False)
     ROLE = [
         ('S', 'SELLER'),
         ('B', 'BIDDER')
     ]
-    role = models.CharField(max_length=1, choices=ROLE, default='B')
+    role = models.CharField(max_length=1, choices=ROLE, blank=False, null=False)
     is_logged_in = models.BooleanField(default=False)
 
 
 class Item(models.Model):
     item_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False, blank=False)
     ITEM_CATEGORY = [
         ('F', 'FURNITURE'),
         ('E', 'ELECTRONICS'),
@@ -38,22 +38,22 @@ class Item(models.Model):
         ('CL', 'CLOSED')
     ]
     item_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    item_category = models.CharField(max_length=4, choices=ITEM_CATEGORY, default='F')
-    item_status = models.IntegerField(max_length=2, choices=ITEM_STATUS, default='NS')
+    item_category = models.CharField(max_length=4, choices=ITEM_CATEGORY, default='A', null=False, blank=False)
+    item_status = models.IntegerField(max_length=2, choices=ITEM_STATUS, default='UP', null=False, blank=False)
 
 
 class Admin(models.Model):
-    id = models.IntegerField(max_length=55, primary_key=True, unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=False, null=False)
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    username = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    password = models.CharField(max_length=20, null=False, blank=False)
     is_logged_in = models.BooleanField(default=False)
 
 
 class Bid(models.Model):
-    id = models.AutoField(primary_key =True, unique=True)
+    id = models.AutoField(primary_key=True)
     item_name = models.CharField(max_length=100)
     bidder = models.CharField(max_length=100)
     ITEM_CATEGORY = [
@@ -71,3 +71,5 @@ class Bid(models.Model):
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(auto_now_add=True)
+    bidder_name = models.CharField(max_length=180, blank=False, null=False)
+
